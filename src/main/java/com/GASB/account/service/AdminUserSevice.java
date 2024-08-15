30801dd3904279df95277144e05eb5abaf0de996
2W <hsp003636@gmail.com>
2024-08-15 13:07:45 +0900

    KAN-350 <add : added salt value when pw hashing>

--- src/main/java/com/GASB/account/service/AdminUserSevice.java
+++ src/main/java/com/GASB/account/service/AdminUserSevice.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+
 @Service
 @RequiredArgsConstructor
 public class AdminUserSevice {
@@ -16,20 +19,41 @@ public class AdminUserSevice {
     private final OrgRepo orgRepository;
     private final PasswordEncoder passwordEncoder;
 
+
+    private static final int SALT_LENGTH = 16;
+
     public void registerAdmin(UserRegistrationRequest request) {
 
         if (adminUserRepository.findByEmail(request.getEmail()) != null) {
             throw new RuntimeException("User already exists");
         }
 
+        // 솔트 생성
+        String salt = generateSalt();
+
+        // 솔트와 비밀번호를 결합하여 해싱
+        String saltedPassword = salt + request.getPassword();
+        String hashedPassword = passwordEncoder.encode(saltedPassword);
+
+        // AdminUsers 엔티티에 솔트와 해시된 비밀번호를 저장
         AdminUsers adminUsers = AdminUsers.builder()
                 .org(orgRepository.findById(request.getOrgId()).orElse(null))
                 .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
+                .password(hashedPassword)
+                .salt(salt) // 솔트를 저장
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .build();
 
         adminUserRepository.save(adminUsers);
+    }
+
+
+
+    private String generateSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[SALT_LENGTH]; // 16바이트 길이의 솔트
+        random.nextBytes(salt);
+        return Base64.getEncoder().encodeToString(salt);
     }
 }
